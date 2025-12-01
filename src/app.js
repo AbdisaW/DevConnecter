aad6cf37b0094fa381dfcae9df82990312038b3d
Abdisa <abdisawake@5gmail.com>
2025-12-01 19:12:50 +0400

    expolring the http methods

--- src/app.js
+++ src/app.js
@@ -3,12 +3,23 @@ const express = require("express");
 const app = express();
 
 
-app.use('/',(req, res)=>{
-    res.send("Hello from Dashbord...")
+
+app.get('/user', (req, res) =>{
+    res.send({firstName: "Abd", lastName: "wak"})
+});
+
+app.post('/user', (req, res)=>{
+    res.send("data saved in database successfuly")
+
+});
+
+app.delete('/user',(req, res)=>{
+    res.send("data delete from databases successfuly ")
 })
-app.use('/test',(req, res)=>{
-    res.send("hello from server...")
+app.use('/test/3',(req, res)=>{
+    res.send("hello from ...")
 })
+
 
 app.listen(3000,(req, res) =>{
     console.log("server listen on port 3000...");
