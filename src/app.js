ca9f6ac23158bf567f17e8b9bdc1c77a470c48b3
Abdisa <abdisawake@5gmail.com>
2025-12-03 11:09:23 +0400

    multiple handler routing

--- src/app.js
+++ src/app.js
@@ -3,25 +3,40 @@ const express = require("express");
 const app = express();
 
 
-
-app.get('/user', (req, res) =>{
-    res.send({firstName: "Abd", lastName: "wak"})
-});
-
-app.post('/user', (req, res)=>{
-    res.send("data saved in database successfuly")
-
-});
-
-app.delete('/user',(req, res)=>{
-    res.send("data delete from databases successfuly ")
-})
-app.use('/test/3',(req, res)=>{
-    res.send("hello from ...")
-})
-
-
-app.listen(3000,(req, res) =>{
-    console.log("server listen on port 3000...");
+app.use('/user', (req, res, next) => {
+    next();
+    console.log("route handler");
     
+    // res.send("response1")
+},
+    (req, res, next) => {
+        console.log("route handler");
+        // res.send("Response2")
+        next()
+
+    },
+     (req, res, next) => {
+        console.log("route handler");
+        // res.send("Response3")
+        next()
+
+    },
+     (req, res, next) => {
+        console.log("route handler");
+        // res.send("Response4")
+        next()
+
+    },
+     (req, res, next) => {
+        console.log("route handler");
+         res.send("Response5")
+        
+
+    },
+);
+
+
+app.listen(3000, (req, res) => {
+    console.log("server listen on port 3000...");
+
 });
