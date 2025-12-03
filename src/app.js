e6a2053c7f81f2f6593d590ee964c88a8b3d2ee6
Abdisa <abdisawake@5gmail.com>
2025-12-03 13:24:10 +0400

    middleware and route

--- src/app.js
+++ src/app.js
@@ -3,34 +3,26 @@ const express = require("express");
 const app = express();
 
 
-app.use('/user', (req, res, next) => {
+app.use('/',(req, res, next)=>{
+    //  res.send("response")
+    next();
+})
+
+app.get('/user', (req, res, next) => {
     next();
     console.log("route handler");
-    
-    // res.send("response1")
+
 },
-    (req, res, next) => {
-        console.log("route handler");
-        // res.send("Response2")
-        next()
-
-    },
+ 
      (req, res, next) => {
         console.log("route handler");
-        // res.send("Response3")
-        next()
+         next()      
 
     },
-     (req, res, next) => {
-        console.log("route handler");
-        // res.send("Response4")
-        next()
 
-    },
      (req, res, next) => {
         console.log("route handler");
-         res.send("Response5")
-        
+         res.send("Response2")
 
     },
 );
