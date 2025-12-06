50403bbfc41b327eac2c05d2ad2b4cc3c9c7d791
Abdisa <abdisawake@5gmail.com>
2025-12-06 23:23:12 +0400

    schema validation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { connectDB } = require('./config/database')
 
-const User = require('./models/user')
+const User = require('./models/user');
 const app = express();
 
 app.use(express.json())
-
+// Regester the user 
 app.post('/signup', async (req, res) => {
 
     const user = new User(req.body);
@@ -19,6 +19,7 @@ app.post('/signup', async (req, res) => {
 
 })
 
+// get user by email
 app.get('/user', async (req, res) => {
     const userEmail = req.body.email;
 
@@ -35,8 +36,8 @@ app.get('/user', async (req, res) => {
         res.status(400).send("somthing want wrong!")
     }
 })
-
-app.get('/feed', async (req, res)=>{
+// get all user
+app.get('/feed', async (req, res) => {
     try {
         const users = await User.find({});
         res.send(users);
@@ -44,7 +45,54 @@ app.get('/feed', async (req, res)=>{
         res.status(400).send("Somthing Want wrong !")
     }
 })
+// get user by id
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const user = await User.findById(id)
+        if (!user) {
+            res.status(404).send("no user found")
+        }
+        else {
+            res.send(user)
+        }
 
+    } catch (err) {
+        res.status(400).send("Somthing Want wrong !")
+    }
+})
+// delete a user from databases
+app.delete('/user', async (req, res) => {
+    const userId = req.body.userId
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            res.status(404).send("user not found")
+        }
+        else {
+            res.send("User deleted successfuly! ")
+        }
+
+    } catch (err) {
+        res.status(400).send("Somthing Want wrong !")
+    }
+})
+
+
+// update data of the user
+
+app.patch('/user', async (req, res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+       await User.findByIdAndUpdate({_id: userId}, data, {runValidators: true});
+       res.send("user updated successfuly")
+
+        
+    } catch (err) {
+        res.status(400).send("user is not updated" + err.message)
+    }
+})
 connectDB().then(() => {
     console.log("Database connection is established")
     app.listen(3000, (req, res) => {
--- src/models/user.js
+++ src/models/user.js
@@ -4,23 +4,34 @@ const mongoose = require('mongoose');
 const userSchema  = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minLength: 4,
+        maxLength:50,
     },
     lastName: {
         type: String,
     },
     email: {
         type: String,
+        required: true,
+        index: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+         min: 18,
     },
     gender: {
         type: String,
+       
     }
 
-});
+},{timestamps: true} );
 
 module.exports = mongoose.model("User", userSchema);
